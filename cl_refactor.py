da7cd53060135219c9fe4a8d99701804449b1e51
vvihorev <vvihorevv@gmail.com>
2021-12-27 11:48:52 +0300

    finished CorpusSearcher class

--- cl_refactor.py
+++ cl_refactor.py
@@ -1,54 +1,58 @@
 import os
-import json
 import pandas as pd
-from src.DocxParser import DocxParser
+from src.CorpusParser import CorpusParser
 
 
+#TODO: go from doc hashes to doc names
 pd.set_option('display.max_columns', None)
 
 
-class CorpusParser:
-    def __init__(self, corpus_path):
-        self.corpus_path = corpus_path
-        self.document_hashes = {}
-        self.hash_count = 0
-        self.parse_corpus()
+class CorpusSearcher:
+    def __init__(self, corpus_parser):
+        if os.path.exists('corpus_text.tsv'):
+            self.corpus_text = pd.read_csv('corpus_text.tsv', sep='\t'))
+        else:
+            self.corpus_text = corpus_parser.corpus_text
 
-    def parse_corpus(self):
-        self._hash_documents()
-        doc_paths = []
-        doc_texts = []
-        img_dirs = []
-        for h in self.document_hashes.keys():
-            doc_path = os.path.join(self.corpus_path, self.document_hashes[h])
-            img_dir = os.path.join('media', str(h))
-            if not os.path.exists(img_dir):
-                os.mkdir(img_dir)
-
-            doc_paths.append(doc_path)
-            doc_text = self._parse_document(doc_path, img_dir)
-            doc_text = doc_text.replace('media/', img_dir+'/')
-            doc_texts.append(doc_text)
-            img_dirs.append(img_dir)
-
-        self.corpus_text = pd.DataFrame({
-            'doc_paths': doc_paths, 
-            'doc_texts': doc_texts,
-            'img_dirs': img_dirs
-            })
 
     def search_corpus(self, query):
-        pass
+        doc_paths = []
+        doc_appearances = []
+        doc_images = []
 
-    def _hash_documents(self):
-        for doc in os.listdir(self.corpus_path):
-            # possibly should add full path
-            self.document_hashes[self.hash_count] = doc
-            self.hash_count += 1
+        for index, row in self.corpus_text.iterrows():
+            if query.lower() in row['doc_text'].lower():
+                doc_text = row['doc_text'].split('\n')
+                appearances = []
+                images = []
+                count = 0
+                for i in range(len(doc_text)):
+                    if query.lower() in doc_text[i].lower():
+                        appearances.append(doc_text[i])
+                        count += 1
+                        images += self.get_relevant_images(doc_text[i:])
 
-    def _parse_document(self, doc_path, img_dir):
-        doc = DocxParser(doc_path, img_dir)
-        return doc.text
+                doc_paths.append(row['doc_path'])
+                doc_appearances.append(appearances)
+                doc_images.append(images)
+
+        res = pd.DataFrame({
+            'doc_path': doc_paths,
+            'doc_appearances': doc_appearances,
+            'doc_images': doc_images
+            })
+        return res
+
+    def get_relevant_images(self, text):
+        count = 0
+        res = []
+        for i in range(len(text)):
+            if count == 2:
+                break
+            if text[i].find('media') != -1:
+                res.append(text[i])
+                count += 1
+        return res
 
 
 class HTMLBuilder:
@@ -58,4 +62,5 @@ class HTMLBuilder:
 
 
 cp = CorpusParser('corpus/')
-print(cp.corpus_text)
+cs = CorpusSearcher(cp)
+print(cs.search_corpus('PA40'))
